e3cd3f4f58bc5c22cdf0bd3e34078cab7396af8c
Jainil Jain <112818722+Jainil2004@users.noreply.github.com>
2023-08-26 02:16:19 +0530

    v1.0.0-alpha1 : improved readability of code

--- Student.java
+++ Student.java
@@ -4,7 +4,7 @@ public class Student {
     private String name; // private - sets the name variable to be only usable in the Student class
     private int sapId;
     private double[] grades;
-    private ArrayList<Course> coursesEnrolled;
+    private ArrayList<Course> coursesEnrolled; //ArrayList to keep track of all the courses a student enrolled in
 
 //    constructor function
     public Student(String name, int sapId) {
@@ -14,17 +14,18 @@ public class Student {
         this.coursesEnrolled = new ArrayList<>();
     }
 
+//    associates the student and the courses together.
     public void enrolledCourses(Course course) {
         coursesEnrolled.add(course);
         course.addStudent(this);
     }
 
-//    Grades setter method
+//    Grades setter method.
     public void setGrades(double[] grades) {
         this.grades = grades;
     }
 
-//    SGPA Calculator method
+//    SGPA Calculator method.
     public double calculateGPA() {
         double total = 0;
         for (double grade : grades) {
@@ -33,14 +34,14 @@ public class Student {
         return total/ grades.length;
     }
 
-//    display relevant information about the student
+//    display relevant information about the student.
     public void displayInfo() {
         System.out.println("SAP ID: " + sapId);
         System.out.println("Student Name: " + name);
         System.out.println("SGPA: " + calculateGPA());
     }
 
-//    list all the courses the student enrolled in
+//    list all the courses the student enrolled in.
     public void listCourses() {
         System.out.print("Courses enrolled in : ");
         for (Course course : coursesEnrolled) {
