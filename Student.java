2d38151fa86d5a9dc954d0576e006b530e298a87
Jainil Jain <112818722+Jainil2004@users.noreply.github.com>
2023-08-26 02:12:16 +0530

    v1.0.0-alpha1 : added Student Class
    Version: v1.0.0alpha1
    
    Uploaded the first main component of the records management System. This class forms the basic structure of the project.
    Please refer to the readme for better understanding of what's new in this update.

---
+++ Student.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Student {
+    private String name; // private - sets the name variable to be only usable in the Student class
+    private int sapId;
+    private double[] grades;
+    private ArrayList<Course> coursesEnrolled;
+
+//    constructor function
+    public Student(String name, int sapId) {
+        this.name = name; // this is used to initialize the name to the object.
+        this.sapId = sapId;
+        this.grades = new double[5];
+        this.coursesEnrolled = new ArrayList<>();
+    }
+
+    public void enrolledCourses(Course course) {
+        coursesEnrolled.add(course);
+        course.addStudent(this);
+    }
+
+//    Grades setter method
+    public void setGrades(double[] grades) {
+        this.grades = grades;
+    }
+
+//    SGPA Calculator method
+    public double calculateGPA() {
+        double total = 0;
+        for (double grade : grades) {
+            total += grade;
+        }
+        return total/ grades.length;
+    }
+
+//    display relevant information about the student
+    public void displayInfo() {
+        System.out.println("SAP ID: " + sapId);
+        System.out.println("Student Name: " + name);
+        System.out.println("SGPA: " + calculateGPA());
+    }
+
+//    list all the courses the student enrolled in
+    public void listCourses() {
+        System.out.print("Courses enrolled in : ");
+        for (Course course : coursesEnrolled) {
+            System.out.print(course.getCourseCode() + " - " + course.getCourseName());
+        }
+        System.out.println("\n");
+    }
+}
--- StudentManagementApp.java
+++ StudentManagementApp.java
@@ -2,10 +2,23 @@ import java.util.Arrays;
 
 public class StudentManagementApp {
     public static void main(String[] args) {
-        System.out.println("The Goal of this Project is to depict the use os OOPs in Java");
-        Student student1 = new Student("Ballu Singh", 500107682);
-        student1.setGrades(new double[] { 4.0, 3.7, 3.5, 3.8, 3.9 });
+        System.out.println("The Goal of this Project is to depict the use os OOPs in Java\n");
+        Course[] courses = DataInitializer.initializeCourses();
+        Student[] students = DataInitializer.initializeStudents();
+        Course[] avaliableCourses = courses.clone();
+        System.out.println("Avaliable Courses");
+        for (Course course : avaliableCourses) {
+            System.out.println(course.getCourseCode() + " - " + course.getCourseName());
+        }
 
-        student1.displayInfo();
+//        System.out.println(avaliableCourses);
+//        courses[0].courseDetails();
+//        courses[1].courseDetails();
+//        courses[2].courseDetails();
+//
+//        students[0].displayInfo();
+//        students[0].enrolledCourses(courses[1]);
+//        students[0].listCourses();
+
     }
 }
