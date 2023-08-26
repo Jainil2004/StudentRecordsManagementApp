cecb8ca0f9863eb9d4762144fbee93ca08c59b58
Jainil Jain <112818722+Jainil2004@users.noreply.github.com>
2023-08-27 02:06:09 +0530

    v1.0.0-alpha2 : integrated DataInitializer
    Version: v1.0.0-alpha2 
    
    Updated the main program for integration of the DataInitializer class in the Project. This class provides all the basic initialization for the program.
    With the addition of this class the program is now capable of some level of functionality.

--- StudentManagementApp.java
+++ StudentManagementApp.java
@@ -1,15 +1,102 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class StudentManagementApp {
     public static void main(String[] args) {
         System.out.println("The Goal of this Project is to depict the use os OOPs in Java\n");
         Course[] courses = DataInitializer.initializeCourses();
         Student[] students = DataInitializer.initializeStudents();
-        Course[] avaliableCourses = courses.clone();
-        System.out.println("Avaliable Courses");
-        for (Course course : avaliableCourses) {
-            System.out.println(course.getCourseCode() + " - " + course.getCourseName());
+        Scanner scanner = new Scanner(System.in);
+        String studentName, mode;
+        Student temp1;
+        byte courseNum;
+        int studentID, selectedCourse;
+        byte iterator = 1;
+        int SAPID, courseID;
+
+        System.out.println(students.length);
+
+//        Menu Driven UI
+        String operation = null;
+
+//        while (true) {
+        while (!"Exit".equals(operation)) {
+
+            System.out.print("Please enter a operation: ");
+            operation = scanner.nextLine();
+
+            switch (operation) {
+                case "newStudent":
+                    System.out.print("Please enter the details of the student you want to add: \n");
+                    System.out.print("Student Name: ");
+                    studentName = scanner.nextLine();
+
+                    System.out.print("Student SAP ID: ");
+                    studentID = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("\n");
+
+                    students[iterator] = new Student(studentName, studentID);
+                    iterator++;
+                    break;
+
+                case "displayStudents":
+                    for (Student student : students) {
+                        if (student != null) {
+                            student.displayInfo();
+                        }
+                    }
+                    break;
+
+                case "avaliableCourses":
+                    UIFeatures.displayAvaliableCourses(courses);
+                    break;
+
+                case "enrolledCourses":
+                    System.out.print("would you like to enroll in a course or list all the courses enrolled in (enrollNew / enrolledCourses): ");
+                    mode = scanner.nextLine();
+//                    scanner.nextLine();
+
+                    System.out.print("SAPID of the Student: ");
+                    SAPID = scanner.nextInt();
+                    scanner.nextLine();
+                    temp1 = UIFeatures.getStudentById(students, SAPID);
+
+                    if ("enrollNew".equals(mode)) {
+                        System.out.print("Select the Course you want to enroll into: ");
+                        selectedCourse = scanner.nextByte();
+                        scanner.nextLine();
+                        temp1.enrolledCourses(courses[selectedCourse]);
+                        System.out.print("Enrollment Successful\n");
+                    }
+                    else if ("enrolledCourses".equals(mode)) {
+                        temp1.listCourses();
+                    }
+                    else {
+                        System.out.print("'"+ mode + "' is not a valid operation\n");
+                    }
+                    break;
+
+                case "courseDetails":
+                    System.out.print("course ID: ");
+                    courseID = scanner.nextInt();
+                    scanner.nextLine();
+                    courseNum = UIFeatures.getCourseById(courses, courseID);
+                    courses[courseNum].courseDetails();
+                    break;
+
+                case "Exit":
+                    System.out.println("Exiting the Program");
+                    break;
+
+                default:
+                    System.out.print("**WARNING: '" + operation + "' is not a valid operation**\n");
+                    break;
+            }
         }
+    }
+}
+
 
 //        System.out.println(avaliableCourses);
 //        courses[0].courseDetails();
@@ -19,6 +106,3 @@ public class StudentManagementApp {
 //        students[0].displayInfo();
 //        students[0].enrolledCourses(courses[1]);
 //        students[0].listCourses();
-
-    }
-}
