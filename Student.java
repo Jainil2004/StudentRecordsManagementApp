e87dbef6e20f5fbd334754ffd2c80917e215d7b1
Jainil Jain <112818722+Jainil2004@users.noreply.github.com>
2023-08-31 20:10:27 +0530

    v1.0.0-alpha3 : added new features

--- Student.java
+++ Student.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Student {
+    Scanner scanner = new Scanner(System.in);
     private String name; // private - sets the name variable to be only usable in the Student class
     private int sapId;
     private double[] grades;
@@ -25,6 +27,26 @@ public class Student {
         this.grades = grades;
     }
 
+    public void addGPA() {
+        double[] originalGPA = new double[coursesEnrolled.size()];
+        double GPAaccquired;
+        byte iterator = 0;
+
+        /* LOGIC
+        * we create a temp array that can store all the values entered by the user
+        * then we call the setGrades method to get all the values in the class variable.
+        * then we call the calculateGPA method to calculate the GPA of the Student*/
+
+        for (Course course : coursesEnrolled) {
+            System.out.println("Score (0 - 5) in " + course.getCourseCode() +" - "+ course.getCourseName() + " course = ");
+            GPAaccquired = scanner.nextDouble();
+            scanner.nextLine();
+            originalGPA[iterator] = GPAaccquired;
+            iterator++;
+        }
+        setGrades(originalGPA);
+    }
+
 //    SGPA Calculator method.
     public double calculateGPA() {
         double total = 0;
@@ -39,6 +61,10 @@ public class Student {
         return sapId;
     }
 
+    public void removeCourse(Course course) {
+        coursesEnrolled.remove(course);
+    }
+
     //    display relevant information about the student.
     public void displayInfo() {
         System.out.println("SAP ID: " + sapId);
@@ -50,13 +76,26 @@ public class Student {
     public void listCourses() {
         System.out.print("SAPID = " + getSapId() + "\n");
         System.out.print("Courses enrolled in: \n");
+
+        boolean hasenrolledCourses = false;
+
         for (Course course : coursesEnrolled) {
-            if (course == null) {
-                System.out.print("Student is not enrolled in any course\n");
-            }
-            else {
+            if (course != null) {
                 System.out.print(course.getCourseCode() + " - " + course.getCourseName() + "\n");
+                hasenrolledCourses = true;
             }
         }
+        if (!hasenrolledCourses) {
+            System.out.println("Student is not enrolled in any course");
+        }
+    }
+
+//    method to check if the particular student is enrolled in a specific course or not
+    public boolean isEnrolledInCourse(Course course) {
+        for (Course enrolledCourse : coursesEnrolled) {
+            if (enrolledCourse != null && enrolledCourse.equals(course));
+            return true;
+        }
+        return false;
     }
 }
